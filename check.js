6d27a6816db88a8c1a80d30c25b12f9cd03e1954
ditpowuh <ditpowuh@gmail.com>
2018-06-22 23:15:17 +1000

    Updated Moblie Check

--- check.js
+++ check.js
@@ -2,7 +2,28 @@
 Copyright © Ditpowuh 2018 Copyright Holder All Rights Reserved | All comments are from me
 */
 
-if (matchMedia("handheld").matches) {
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if (isMobile.any()) {
     window.open("mobile.html", "_self");
 }
 else {
